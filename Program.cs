5ddd73f3a8bc4bffdc8f9ccfb46dbeff4bfab17b
Dmitry Samodov <samodov.dmitry@gmail.com>
2016-11-30 14:53:59 +0300

    removed default settings

--- Program.cs
+++ Program.cs
@@ -57,55 +57,67 @@ namespace EmailSenderBridge
             if (string.IsNullOrEmpty(settings.ServiceBus.NamespaceUrl))
             {
                 isValid = false;
-                Console.WriteLine("Provide NamespaceUrl value for ServiceBus in appsettings.json or ServiceBus:NamespaceUrl from env variable");
+                Console.WriteLine("Provide NamespaceUrl value for ServiceBus section in appsettings.json or ServiceBus:NamespaceUrl from env variable");
             }
 
             if (string.IsNullOrEmpty(settings.ServiceBus.PolicyName))
             {
                 isValid = false;
-                Console.WriteLine("Provide PolicyName value for ServiceBus in appsettings.json or ServiceBus:PolicyName from env variable");
+                Console.WriteLine("Provide PolicyName value for ServiceBus section in appsettings.json or ServiceBus:PolicyName from env variable");
             }
 
             if (string.IsNullOrEmpty(settings.ServiceBus.Key))
             {
                 isValid = false;
-                Console.WriteLine("Provide Key value for ServiceBus in appsettings.json or ServiceBus:Key from env variable");
+                Console.WriteLine("Provide Key value for ServiceBus section in appsettings.json or ServiceBus:Key from env variable");
             }
 
             if (string.IsNullOrEmpty(settings.ServiceBus.QueueName))
             {
                 isValid = false;
-                Console.WriteLine("Provide QueueName value for ServiceBus in appsettings.json or ServiceBus:QueueName from env variable");
+                Console.WriteLine("Provide QueueName value for ServiceBus section in appsettings.json or ServiceBus:QueueName from env variable");
             }
 
             if (string.IsNullOrEmpty(settings.Smtp.Host))
             {
                 isValid = false;
-                Console.WriteLine("Provide Host value for Smtp in appsettings.json or Smtp:Host from env variable");
+                Console.WriteLine("Provide Host value for Smtp section in appsettings.json or Smtp:Host from env variable");
             }
 
             if (settings.Smtp.Port == 0)
             {
                 isValid = false;
-                Console.WriteLine("Provide Post value for Smtp in appsettings.json or Smtp:Port from env variable");
+                Console.WriteLine("Provide Post value for Smtp section in appsettings.json or Smtp:Port from env variable");
             }
 
             if (string.IsNullOrEmpty(settings.Smtp.Login))
             {
                 isValid = false;
-                Console.WriteLine("Provide Login value for Smtp in appsettings.json or Smtp:Login from env variable");
+                Console.WriteLine("Provide Login value for Smtp section in appsettings.json or Smtp:Login from env variable");
             }
 
             if (string.IsNullOrEmpty(settings.Smtp.Password))
             {
                 isValid = false;
-                Console.WriteLine("Provide Password value for Smtp in appsettings.json or Smtp:Password from env variable");
+                Console.WriteLine("Provide Password value for Smtp section in appsettings.json or Smtp:Password from env variable");
             }
 
             if (string.IsNullOrEmpty(settings.Smtp.From))
             {
                 isValid = false;
-                Console.WriteLine("Provide From value for Smtp in appsettings.json or Smtp:From from env variable");
+                Console.WriteLine("Provide From value for Smtp section in appsettings.json or Smtp:From from env variable");
+            }
+
+            if (string.IsNullOrEmpty(settings.Smtp.DisplayName))
+            {
+                isValid = false;
+                Console.WriteLine("Provide DisplayName value for Smtp section in appsettings.json or Smtp:DisplayName from env variable");
+            }
+
+            if (string.IsNullOrEmpty(settings.Smtp.LocalDomain))
+            {
+                isValid = false;
+                Console.WriteLine("Provide LocalDomain value for Smtp section in appsettings.json or Smtp:LocalDomain from env variable");
             }
 
             return isValid;
